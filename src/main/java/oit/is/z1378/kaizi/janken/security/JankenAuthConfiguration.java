4da8f1a72125bb67015f22c7df4bad5e0258f5b5
Shiraishi-421 <e1b20052@oit.ac.jp>
2022-10-16 01:03:48 +0900

    認可処理の追加

--- src/main/java/oit/is/z1378/kaizi/janken/security/JankenAuthConfiguration.java
+++ src/main/java/oit/is/z1378/kaizi/janken/security/JankenAuthConfiguration.java
@@ -59,13 +59,13 @@ public class JankenAuthConfiguration {
     // Spring Securityのフォームを利用してログインを行う（自前でログインフォームを用意することも可能）
     http.formLogin();
 
-    // http://localhost:8000/sample3 で始まるURLへのアクセスはログインが必要
+    // http://localhost:8080/janken で始まるURLへのアクセスはログインが必要
     // mvcMatchers().authenticated()がmvcMatchersに指定されたアクセス先に認証処理が必要であることを示す
     // authenticated()の代わりにpermitAll()と書くと認証不要となる
     http.authorizeHttpRequests()
-        .mvcMatchers("/sample3/**").authenticated();
+        .mvcMatchers("/janken/**").authenticated();
 
-    http.logout().logoutSuccessUrl("/"); // ログアウト時は "http://localhost:8000/" に戻る
+    http.logout().logoutSuccessUrl("/"); // ログアウト時は "http://localhost:8080/" に戻る
     return http.build();
   }
 
